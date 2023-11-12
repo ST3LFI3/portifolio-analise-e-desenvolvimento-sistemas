88f272be892e81845dfb70d206a6252eb1ac61f4
ST3LFI3 <gabrielbangarclprates@gmail.com>
2023-11-11 20:42:17 -0400

    adicionando projeto

---
+++ Portifolio/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Portifolio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.A = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.B = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.C = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.D = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.E = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(35, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(409, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A)Apresentar todos os valores numéricos inteiros ímpares situados na faixa de 0 a" +
+    " 40.\r\n\r\n";
+      
+            // 
+            // A
+            // 
+            this.A.AutoSize = true;
+            this.A.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.A.Location = new System.Drawing.Point(35, 59);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(52, 13);
+            this.A.TabIndex = 1;
+            this.A.Text = "Resposta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(35, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(455, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "B)Apresentar o total da soma dos 100 primeiros números inteiros, representados pe" +
+    "la sequência\r\n1+2+3+4...+98+99+100\r\n";
+            // 
+            // B
+            // 
+            this.B.AutoSize = true;
+            this.B.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.B.Location = new System.Drawing.Point(35, 120);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(52, 13);
+            this.B.TabIndex = 3;
+            this.B.Text = "Resposta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(35, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(520, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "c) Apresentar todos os números divisíveis por 4 que sejam menores que 40. Iniciar" +
+    " a contagem com o valor 1\r\n(um).\r\n";
+            // 
+            // C
+            // 
+            this.C.AutoSize = true;
+            this.C.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.C.Location = new System.Drawing.Point(35, 169);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(52, 13);
+            this.C.TabIndex = 5;
+            this.C.Text = "Resposta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(35, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(299, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "d) Apresentar os quadrados dos números inteiros de 15 a 200.";
+            // 
+            // D
+            // 
+            this.D.AutoSize = true;
+            this.D.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.D.Location = new System.Drawing.Point(35, 216);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(52, 13);
+            this.D.TabIndex = 7;
+            this.D.Text = "Resposta";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label7.Location = new System.Drawing.Point(35, 337);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(479, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "e) Apresentar o resultado da soma de todos os valores pares existentes na faixa n" +
+    "umérica de 1 a 50.\r\n";
+            // 
+            // E
+            // 
+            this.E.AutoSize = true;
+            this.E.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.E.Location = new System.Drawing.Point(35, 359);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(52, 13);
+            this.E.TabIndex = 9;
+            this.E.Text = "Resposta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label4.Location = new System.Drawing.Point(35, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ALUNO GABRIEL NEVES";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.E);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label A;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label B;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label C;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label D;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label E;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Portifolio/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Portifolio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            QuestaoA(40,this);
+            QuestaoB(this);
+            QuestaoC(this);
+            QuestaoD(this);
+            QuestaoE(this);
+        }
+
+      
+        static void QuestaoA(int limite, Form1 form)
+        {
+            string result = "";
+            for (int i = 1; i <= limite; i += 2)
+            {
+                Console.Write(i + " ");
+                result = result + "," + i.ToString();
+            }
+            form.A.Text = "RESPOSTA : " + result;
+        }
+        static void QuestaoB(Form1 form)
+        {
+            int soma = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += i;
+            }
+
+ 
+            form.B.Text = "RESPOSTA : " + soma.ToString();
+        }
+        static void QuestaoC(Form1 form)
+        {
+            string result = "";
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    result = result + "," + i.ToString();
+                }
+            }
+
+            form.C.Text = "RESPOSTA : " +  result;
+        }
+        static void QuestaoD(Form1 form)
+        {
+
+            string quadrados = "";
+
+            for (int i = 15; i <= 200; i++)
+            {
+                quadrados += (i * i).ToString();
+
+                if (i < 200)
+                {
+                    quadrados += ",";
+                }
+
+                if (i % 30 == 0 && i < 200)
+                {
+                    quadrados += "\n";
+                }
+            }
+            form.D.Font = new Font(form.D.Font.FontFamily, 10);
+            form.D.Text = "RESPOSTA : " + quadrados;
+        }
+        static void QuestaoE(Form1 form)
+        {
+            int soma = 0;
+
+            for (int i = 1; i <= 50; i++)
+            {
+               
+                if (i % 2 == 0)
+                {
+                    soma += i;
+                }
+            }
+
+            form.E.Text = "RESPOSTA : "+ soma.ToString(); ;
+        }
+    }
+}
